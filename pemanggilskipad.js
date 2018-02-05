125a8f49d1924fdc1b85e11cf2ba33a434b181f9
rizkyheryandi <33335132+rizkyheryandi@users.noreply.github.com>
2018-02-05 20:43:57 +0700

    Update pemanggilskipad.js

--- pemanggilskipad.js
+++ pemanggilskipad.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/01/ges-insurance-liabilities-return-to.html"}
+if(x=="1"){xxx="/2018/02/car-insurance-secrets-8-secrets-your.html"}
 if(x=="2"){xxx="/2018/01/farmers-insurance-open-five-takeaways.html"}
 if(x=="3"){xxx="/2018/01/boc-pays-womans-asbestos-caused-cancer.html"}
-if(x=="4"){xxx="/2018/01/combat-mesothelioma-risks-by-avoiding.html"}
-if(x=="5"){xxx="/2018/01/canadian-mesothelioma-survivor-advocate.html"}a_to_ve[i].href="http://www.skipad.online"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/2018/02/homeowners-insurance-quotes.html"}
+if(x=="5"){xxx="/2018/02/insurance-on-your-personal-loan.html"}a_to_ve[i].href="http://www.skipad.online"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
