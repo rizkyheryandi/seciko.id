ad7f67ee4cbc81befca7840f476f9da41d8f446c
rizkyheryandi <33335132+rizkyheryandi@users.noreply.github.com>
2018-02-05 20:37:18 +0700

    Update Post4skipad.js

--- Post4skipad.js
+++ Post4skipad.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://www.skipad.online/2018/01/combat-mesothelioma-risks-by-avoiding.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://www.skipad.online/2018/02/homeowners-insurance-quotes.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
