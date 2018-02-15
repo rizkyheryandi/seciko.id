b50304c3f043715ad4b72bffe5f7cdd33807ffef
rizkyheryandi <33335132+rizkyheryandi@users.noreply.github.com>
2018-02-15 20:08:14 +0700

    Update safelinkv2utama.js

--- safelinkv2utama.js
+++ safelinkv2utama.js
@@ -19,13 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="how-to-save-money-on-car-insurance-in.html"}
 if(x=="2"){xxx="the-best-consideration-is-auto-insurance.html"}
 if(x=="3"){xxx="car-insurance-secrets-8-secrets-your.html"}
 if(x=="4"){xxx="homeowners-insurance-quotes.html"}
-if(x=="5"){xxx="insurance-on-your-personal-loan.html"}a_to_ve[i].href="http://www.skipad.online/2018/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="insurance-on-your-personal-loan.html"}
+if(x=="6"){xxx="asuransi-perjalanan-terbaik-di.html"}a_to_ve[i].href="http://www.skipad.online/2018/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
